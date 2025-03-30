e2ef991958957a3040a43f972232e91894770070
Muller1616 <mulugetaabeje16@gmail.com>
2025-03-30 12:27:00 +0300

    Added new folders and solved problems (Basics/small.py)

--- 2469_Convert_Celsius.py
+++ 2469_Convert_Celsius.py
@@ -27,13 +27,11 @@
 # 0 <= celsius <= 1000
 
 
-from typing import List 
-
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
         kelvin = celsius + 273.15  
         fahrenheit = celsius * 1.80 + 32.00
-        return [round(kelvin, 5), round(fahrenheit, 5)]  
+        return [round(kelvin, 5), round(fahrenheit, 5)] 
 
 solution = Solution()
 print(solution.convertTemperature(36.5))
