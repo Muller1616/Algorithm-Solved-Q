243495d148545426ed3a1a708536483ab5e3c2ba
Mulugeta <mulugetaabeje16@gmail.com>
2025-04-16 00:04:16 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        a = ''
+        ls = []
+
+        for digit in digits:
+            a += str(digit)
+        for d in a:    
+          
+            ls.append(c)
+        return c
