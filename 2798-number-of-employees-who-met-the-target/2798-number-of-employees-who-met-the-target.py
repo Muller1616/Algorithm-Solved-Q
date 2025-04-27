d1b5dbbc4ef4d9228bf3b1a15f328bccdc868f5b
Mulugeta <mulugetaabeje16@gmail.com>
2025-04-27 11:14:57 +0300

    Time: 0 ms (100%), Space: 17.7 MB (83.6%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        n = len(hours)
+        count = 0
+        for i in range(n):
+            if hours[i] >= target:
+                count += 1
+        return count
+        
+        
