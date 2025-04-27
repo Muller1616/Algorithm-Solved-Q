802422d8e33858f663e7bc338c80002d9093dd0b
Mulugeta <mulugetaabeje16@gmail.com>
2025-04-27 12:48:14 +0300

    Time: 30 ms (27.27%), Space: 17.9 MB (41.87%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        re = 0
+        for i in range(1,n+1):
+            if i%3 == 0:
+                re += i
+            elif i%5 == 0:
+                re += i
+            elif i%7 == 0:
+                re += i
+        return re
+        
