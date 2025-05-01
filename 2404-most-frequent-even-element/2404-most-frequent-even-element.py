e375f963887889a899d7a7309796494f59539925
Mulugeta <mulugetaabeje16@gmail.com>
2025-05-02 01:03:41 +0300

    Time: 155 ms (10.94%), Space: 18.1 MB (82.62%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -2,15 +2,13 @@ from collections import Counter
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
         li = []
+        nums.sort()
         for i in range(len(nums)):
             if nums[i] % 2 == 0:
                 li.append(nums[i])
-        if not li:
-            return -1
-        fre = Counter(li)
-        m = max(fre.values())
-        c = [num for num in fre if fre[num] == m]
-        return min(c)
-
-
+        li = Counter(li)
+        for key, value in li.items():
+            if value ==max(li.values()):
+                return key
+        return -1
         
