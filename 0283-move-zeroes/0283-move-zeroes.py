3bf703a0ec7f54a79716e652af12178c178ca563
Mulugeta <mulugetaabeje16@gmail.com>
2025-10-29 23:20:29 +0300

    Time: 7 ms (43.21%), Space: 19.1 MB (9.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        pos = 0
-        for i in range(n):
-            if nums[i] != 0:
-                nums[pos] = nums[i]
-                pos += 1
-        for i in range(pos,n):
-            nums[i] = 0
-                
+        left = 0 
+        right = 1
+        while  right < len(nums):
+            if nums[right] != 0 and nums[left] == 0:
+                temp = nums[left]
+                nums[left] = nums[right]
+                nums[right] = temp
+                left += 1
+            if nums[left] != 0:
+                left += 1
+            right += 1
+        return nums
+
         
